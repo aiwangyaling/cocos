63aeb43a0105b280aca01516c1c3e549a3741d85
pengxianfeng <xianfeng@peng-xian-feng-de-MacBook-Pro.local>
2016-10-14 12:05:40 +0800

    实验

---
+++ uppen1/frameworks/runtime-src/Classes/native/CCAlertViewDelegate.h
@@ -0,0 +1,18 @@
+
+#ifndef __CC_EXTENSION_CCALERTVIEW_DELEGATE_H_
+#define __CC_EXTENSION_CCALERTVIEW_DELEGATE_H_
+
+//#include "cocos2dx_extra.h"
+
+//NS_CC_EXTRA_BEGIN
+namespace mybo{
+class AlertViewDelegate
+{
+public:
+    virtual void alertViewClickedButtonAtIndex(int buttonIndex) = 0;
+};
+}
+
+//NS_CC_EXTRA_END
+
+#endif // __CC_EXTENSION_CCALERTVIEW_DELEGATE_H_
